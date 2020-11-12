5400e106eb921cbffa3c71d683cae2799daa0624
mleader <mleader@redhat.com>
2020-11-12 10:31:39 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from enum import Enum
+
+import panel as pn
+import elasticsearch as es
+import pandas as pd
+import pandasticsearch as pdsh
+
+
+class PipelineStatus(Enum):
+    red = 'failure'
+    yellow = 'unstable'
+    green = 'success'
+
+
+def es_data():
+    url = os.getenv('ELASTICSEARCH_URL')
+    index = 'mb'
+    db = es.Elasticsearch(
+        url
+    )   
+    table = db.search(index='mb')
+    return table
+
+
+def dummy_data():
+    return pd.DataFrame.from_records(
+        data=(
+            ('aws', 'failure'),
+            ('aws future', 'succes'),
+            ('aws next', 'unstable'),
+            ('aws ovn next', 'success'),
+            ('azure', 'failure'),
+            ('gcp', 'failure')
+        ),
+        columns=['pipeline', 'status']
+    )
+
+
+
+def main():
+    table = es_data()
+    mb = pdsh.Select.from_dict(table).to_pandas()
+    df0 = dummy_data()
+
+
+
+if __name__ == '__main__':
+    main()
